837e0cb7c26913508612d86616f3e7bb50f83828
Copple <10214025+kiwicopple@users.noreply.github.com>
2024-04-07 13:10:40 +0800

    adds the patch set

--- src/components/HomepageSections/index.tsx
+++ src/components/HomepageSections/index.tsx
@@ -19,16 +19,34 @@ export default function HomepageSections(): JSX.Element {
 
           <div className="row">
             <div className="col col--6">
-              <img
-                src="/img/site/read-only-results-amazon.png"
-                alt="Scalability"
-              />
+              <div className="">
+                <div className="">
+                  <img
+                    src="/img/site/read-only-results-amazon.png"
+                    alt="Scalability"
+                  />
+                </div>
+                <div className="text--center">
+                  <p>
+                    Read-only tests show that the OrioleDB storage format is
+                    over to 4x faster than PostgreSQL's current storage format.
+                  </p>
+                </div>
+              </div>
             </div>
             <div className="col col--6">
-              <img
-                src="/img/site/read-only-results-amazon.png"
-                alt="Scalability"
-              />
+              <div className="">
+                <img
+                  src="/img/site/read-write-results-amazon.png"
+                  alt="Scalability"
+                />
+              </div>
+              <div className="text--center">
+                <p>
+                  Read-write tests show that the OrioleDB storage format is over
+                  to 4.5x faster than PostgreSQL's current storage format.
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -41,19 +59,37 @@ export default function HomepageSections(): JSX.Element {
             </Heading>
 
             <p>
-              OrioleDB’s architecture dramatically{" "}
+              OrioleDB's architecture dramatically{" "}
               <strong>reduces resources</strong> used for transaction
               processing.
             </p>
           </div>
           <div className="row">
             <div className="col col--6">
-              <div className="card__image">
-                <img src="/img/site/juno-read-iops.png" alt="Scalability" />
+              <div className="">
+                <div className="">
+                  <img src="/img/site/juno-read-iops.png" alt="Scalability" />
+                </div>
+                <div className="text--center">
+                  <p>
+                    OrioleDB is over 6x more efficient on disk reads / IOPS than
+                    the current PostgreSQL storage format.
+                  </p>
+                </div>
               </div>
             </div>
             <div className="col col--6">
-              <img src="/img/site/juno-write-iops.png" alt="Scalability" />
+              <div className="">
+                <div className="">
+                  <img src="/img/site/juno-write-iops.png" alt="Scalability" />
+                </div>
+                <div className="text--center">
+                  <p>
+                    OrioleDB results in a dramatic reduction in disk writes
+                    compared to the current PostgreSQL storage format.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>

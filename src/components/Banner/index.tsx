495e5c0219ef600f220516f3f5ac150437a75747
Copple <10214025+kiwicopple@users.noreply.github.com>
2024-04-07 12:09:50 +0800

    fix layout on mobile

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,55 @@
+import clsx from "clsx"
+import Heading from "@theme/Heading"
+import Link from "@docusaurus/Link"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+
+import styles from "./styles.module.css"
+
+export default function HomepageBanner(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext()
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <div className="row">
+          <div className="col col--6">
+            <Heading as="h2" className={clsx("hero__title", styles.heroTitle)}>
+              {siteConfig.tagline}
+            </Heading>
+            <p className={clsx("hero__subtitle", styles.heroSubtitle)}>
+              OrioleDB is a PostgreSQL extension that combines the advantages of
+              both on-disk and in-memory engines. Increase your database
+              performance and cut costs.
+            </p>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx(
+                  "button button--secondary button--lg",
+                  styles.primaryButton
+                )}
+                to="/docs"
+              >
+                Read the docs
+              </Link>
+              <Link
+                className={clsx(
+                  "secondary-button button button--secondary button--outline button--lg",
+                  styles.secondaryButton
+                )}
+                to="https://github.com/orioledb/orioledb"
+              >
+                GitHub
+              </Link>
+            </div>
+          </div>
+          <div className={clsx("col col--6", styles.heroImageContainer)}>
+            <img
+              className={styles.heroImage}
+              src="/img/banner.png"
+              alt="OrioleDB"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,55 +2,12 @@ import clsx from "clsx"
 import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
+import Heading from "@theme/Heading"
+
+import Banner from "@site/src/components/Banner"
 import HomepageFeatures from "@site/src/components/HomepageFeatures"
 import HomepageSections from "@site/src/components/HomepageSections"
 import CTA from "@site/src/components/CTA"
-import Heading from "@theme/Heading"
-
-import styles from "./index.module.css"
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <div className="row">
-          <div className="col col--6 padding--lg">
-            <Heading as="h2" className="hero__title">
-              {siteConfig.tagline}
-            </Heading>
-            <p className="hero__subtitle">
-              OrioleDB is a PostgreSQL extension that combines the advantages of
-              both on-disk and in-memory engines. Increase your database
-              performance and cut costs.
-            </p>
-            <div className={styles.buttons}>
-              <Link className="button button--secondary button--lg" to="/docs">
-                Read the docs
-              </Link>
-              <Link
-                className={clsx(
-                  "secondary-button button button--secondary button--outline button--lg",
-                  styles.secondaryButton
-                )}
-                to="https://github.com/orioledb/orioledb"
-              >
-                GitHub
-              </Link>
-            </div>
-          </div>
-          <div className="col col--6 padding-top--lg">
-            <img
-              className={styles.heroImage}
-              src="/img/banner.png"
-              alt="OrioleDB"
-            />
-          </div>
-        </div>
-      </div>
-    </header>
-  )
-}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
@@ -59,8 +16,8 @@ export default function Home(): JSX.Element {
       title={`${siteConfig.title}`}
       description="OrioleDB is a PostgreSQL extension that combines the advantages of both on-disk and in-memory engines."
     >
-      <HomepageHeader />
       <main>
+        <Banner />
         <HomepageFeatures />
         <HomepageSections />
         <CTA />
